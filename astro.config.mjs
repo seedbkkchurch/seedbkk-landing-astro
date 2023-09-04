4101167b3ca554c9032ae2c16b8ca7dca29472a0
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2023-09-04 17:38:49 +0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
   site: 'seedbkkchurch.github.io',
-=======
-  site: 'https://seedbkkchurch.github.io/seedbkk-landing-astro',
->>>>>>> 1a35a8d9dddd8b43264a0c877e6bfd8698c3575d
   base: '/seedbkk-landing-astro',
   integrations: [mdx(), sitemap(), image()]
 });
