7b336a3f4fc028375715c2dbfbe216895603cf3e
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2024-09-18 10:56:07 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import image from "@astrojs/image";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   //site: 'https://example.com',
   site: 'https://www.seedchurchbkk.org',
-
-  integrations: [mdx(), sitemap(), image()]
+  integrations: [mdx(), sitemap(), image(), react()]
 });
