3569a3b1f39690335da3253b1c4780bbc879389c
Nantipat <keymasterviriya1150@gmail.com>
2025-02-11 14:44:50 +0700

    refactor(navigation): update header and footer
    - Removed unused navigation links
    - Added new lesson links to header
    - Updated footer copyright information
    - Simplified navigation structure
    - Removed several unused links

--- src/navigation.ts
+++ src/navigation.ts
@@ -59,10 +59,10 @@ export const headerData = {
     {
       text: 'Event',
       links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
+        // {
+        //   text: 'Lead Generation',
+        //   href: getPermalink('/landing/lead-generation'),
+        // },
         // {
         //   text: 'Long-form Sales',
         //   href: getPermalink('/landing/sales'),
@@ -110,15 +110,20 @@ export const headerData = {
         // },
       ],
     },
-
     {
-      text: 'มานาอาจารย์ใหญ่',
-      href: 'https://youtu.be/gi2aAwn_Xtc?si=Bg3IygEx1gQiwmHj',
+      text: 'บทเรียน',
+      links: [
+        {
+          text: 'มานาอาจารย์ใหญ่',
+          href: 'https://youtu.be/gi2aAwn_Xtc?si=Bg3IygEx1gQiwmHj',
+        },
+        {
+          text:'บทเรียนผู้เชื่อใหม่',
+          href:'https://youtu.be/UYIhjhNNRxU?si=EJB-fBWSrDq5x9el'
+        }
+      ]
     },
-    {
-      text:'บทเรียนผู้เชื่อใหม่',
-      href:'https://youtu.be/UYIhjhNNRxU?si=EJB-fBWSrDq5x9el'
-    }
+    
   ],
   actions: [{ text: 'รู้จักพระเจ้า', href: '', target: '_blank' }],
 };
@@ -126,56 +131,56 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: '',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        // { text: 'Features', href: '#' },
+        // { text: 'Security', href: '#' },
+        // { text: 'Team', href: '#' },
+        // { text: 'Enterprise', href: '#' },
+        // { text: 'Customer stories', href: '#' },
+        // { text: 'Pricing', href: '#' },
+        // { text: 'Resources', href: '#' },
       ],
     },
     {
-      title: 'Platform',
+      title: '',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        // { text: 'Developer API', href: '#' },
+        // { text: 'Partners', href: '#' },
+        // { text: 'Atom', href: '#' },
+        // { text: 'Electron', href: '#' },
+        // { text: 'AstroWind Desktop', href: '#' },
       ],
     },
     {
-      title: 'Support',
+      title: '',
       links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
+        // { text: 'Docs', href: '#' },
+        // { text: 'Community Forum', href: '#' },
+        // { text: 'Professional Services', href: '#' },
+        // { text: 'Skills', href: '#' },
+        // { text: 'Status', href: '#' },
       ],
     },
     {
-      title: 'Company',
+      title: '',
       links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        // { text: 'About', href: '#' },
+        // { text: 'Blog', href: '#' },
+        // { text: 'Careers', href: '#' },
+        // { text: 'Press', href: '#' },
+        // { text: 'Inclusion', href: '#' },
+        // { text: 'Social Impact', href: '#' },
+        // { text: 'Shop', href: '#' },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    // { text: 'Terms', href: getPermalink('/terms') },
+    // { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
@@ -183,6 +188,6 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> NantipatSoftEn </a> · All rights reserved.
   `,
 };
