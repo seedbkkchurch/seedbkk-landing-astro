24dd51e8ce37b9779c29efe7937724f7b4028421
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2025-03-16 13:39:17 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,11 +35,6 @@ export default {
                     },
                 },
             },
-            borderRadius: {
-                lg: 'var(--radius)',
-                md: 'calc(var(--radius) - 2px)',
-                sm: 'calc(var(--radius) - 4px)',
-            },
         },
     },
     plugins: [
