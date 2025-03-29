ebd7caf9d637ffa49105c177cdc0108e8f666bea
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2025-03-29 18:46:13 +0700

    chore(assets): add new cover image
    - Added 465074965_1089869412890123_3866926757510678653_n.j

--- src/navigation.ts
+++ src/navigation.ts
@@ -17,10 +17,10 @@ export const headerData = {
                     text: 'Mobile App',
                     href: getPermalink('/homes/mobile-app'),
                 },
-                // {
-                //     text: 'Pastor',
-                //     href: getPermalink('/homes/pastor'),
-                // },
+                {
+                    text: 'About us',
+                    href: getPermalink('/homes/about'),
+                },
                 {
                     text: 'culture',
                     href: getPermalink('/homes/culture'),
