dd4d1aaca52c2da03de90fde958034c4838e9b5b
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2025-08-10 14:26:36 +0700

    update

---
+++ src/const/Links.ts
@@ -0,0 +1,12 @@
+export const Links = {
+    github: 'https://github.com/seedbkkchurch/seedbkk-landing-astro',
+    facebook: 'https://www.facebook.com/seedbkk.th',
+    instagram: 'https://www.instagram.com/seedbkk.th/',
+    youtube: 'https://www.youtube.com/@seedbangkok',
+    admin: 'https://github.com/NantipatSoftEn',
+    email: 'seedch.bkk@gmail.com',
+    telephone: '064 992 2255',
+    location: 'https://maps.app.goo.gl/wdg1pUCeVKRm5Khm9',
+    website: 'https://seedbkkchurch.com',
+    tiktok: 'https://www.tiktok.com/@codeseeds',
+}
--- src/navigation.ts
+++ src/navigation.ts
@@ -1,5 +1,5 @@
+import { Links } from './const/Links'
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks'
-
 export const headerData = {
     links: [
         {
@@ -197,14 +197,14 @@ export const footerData = {
         // { text: 'Privacy Policy', href: getPermalink('/privacy') },
     ],
     socialLinks: [
-        { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: '#' },
-        { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-        { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+        { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: Links.youtube },
+        { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: Links.instagram },
+        { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: Links.facebook },
         { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-        { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+        { ariaLabel: 'Github', icon: 'tabler:brand-github', href: Links.github },
     ],
     footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="/icons/SEED_CHURCH_LOGO.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> NantipatSoftEn </a> · All rights reserved.
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/NantipatSoftEn"> NantipatSoftEn </a> · All rights reserved.
   `,
 }
